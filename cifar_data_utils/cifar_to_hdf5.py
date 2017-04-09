dcf9c4164d125975a071b9317512c6efde7b28cb
Mohit Sharma <mohit@Mohits-MBP.wv.cc.cmu.edu>
2017-04-08 20:51:33 -0400

    Convert cifar 10 batch data to usual hdf5.

---
+++ cifar_data_utils/cifar_to_hdf5.py
@@ -0,0 +1,118 @@
+import numpy as np
+import h5py
+import csv
+import pdb
+import os
+import argparse
+import pprint
+import types
+
+def recursively_save_dict_contents_to_group(h5file, path, dic):
+  """
+  Take an already open HDF5 file and insert the contents of a dictionary
+  at the current path location. Can call itself recursively to fill
+  out HDF5 files with the contents of a dictionary.
+  """
+  assert type(dic) is types.DictionaryType, "must provide a dictionary"
+  assert type(path) is types.StringType, "path must be a string"
+  assert type(h5file) is h5py._hl.files.File, "must be an open h5py file"
+  for key in dic:
+    assert type(key) is types.StringType, \
+    'dict keys must be strings to save to hdf5'
+    if type(dic[key]) in (np.int64, np.float64, types.StringType):
+      h5file[path + key] = dic[key]
+      assert h5file[path + key].value == dic[key], \
+        'The data representation in the HDF5 file does not match the ' \
+            'original dict.'
+    if type(dic[key]) is np.ndarray:
+      h5file[path + key] = dic[key]
+      assert np.array_equal(h5file[path + key].value, dic[key]), \
+          'The data representation in the HDF5 file does not match the ' \
+              'original dict.'
+    elif type(dic[key]) is types.DictionaryType:
+      recursively_save_dict_contents_to_group(
+          h5file, path + key + '/', dic[key])
+
+def debug_visualize(d):
+  from PIL import Image
+  d = np.array(d, dtype=np.int8)
+  d = np.swapaxes(d, 0, 1)
+  d = np.swapaxes(d, 1, 2)
+  img = Image.fromarray(d, 'RGB')
+  img.show()
+  pdb.set_trace()
+
+def unpickle(f):
+    import cPickle
+    with open(f, 'rb') as fo:
+        d = cPickle.load(fo)
+    return d
+
+def convert_data_to_3d(d):
+  '''
+  d: Nx3072 where 1024 [R,G,B] -- 32*32 [R,G,B] (row major) 
+  '''
+  d_r, d_g, d_b = np.array(d[:,:1024]), np.array(d[:, 1024:2048]), \
+      np.array(d[:,2048:])
+  N = d_r.shape[0]
+  data = np.zeros((N, 3, 32, 32))
+  data[:,0,:,:] = np.reshape(d_r, (N, 32, 32))
+  data[:,1,:,:] = np.reshape(d_g, (N, 32, 32))
+  data[:,2,:,:] = np.reshape(d_b, (N, 32, 32))
+  
+  # Visualize random elements to see that we have proper images
+  # debug_visualize(data[0])
+
+  return data
+
+
+def main(dir_pkl, dir_h5):
+  '''
+  dir_pkl: read data_batch_[1/5], test_batch
+  dir_h5: write train_data.h5, test_data.h5
+  '''
+  train_data = [f for f in os.listdir(dir_pkl) if f.startswith('data_batch')]
+  test_data = [f for f in os.listdir(dir_pkl) if f.startswith('test_batch')]
+  train_data_h5 = {'data': [], 'labels': []}
+  for f in train_data:
+    f_data = unpickle(os.path.join(dir_pkl, f))
+    train_data_h5['data'] = convert_data_to_3d(f_data['data'])
+    train_data_h5['labels'] = np.array(f_data['labels'], dtype=np.int32)
+
+  train_h5_path = os.path.join(dir_h5, 'train_data.h5')
+  train_h5 = h5py.File(train_h5_path, 'w')
+  train_data_h5['data'] = np.hstack(train_data_h5['data'])
+  train_data_h5['labels'] = np.hstack(train_data_h5['labels'])
+  recursively_save_dict_contents_to_group(train_h5, '/', train_data_h5)
+  train_h5.flush()
+  train_h5.close()
+  print('Did write {}'.format(train_h5_path))
+
+  # There is only test pkl file
+  assert(len(test_data) == 1)
+  test_data_h5 = {}
+  test_data = unpickle(os.path.join(dir_pkl, test_data[0]))
+  test_data['data'] = convert_data_to_3d(test_data['data'])
+  test_data['labels'] = np.array(test_data['labels'], dtype=np.int32)
+  test_h5_path = os.path.join(dir_h5, 'test_data.h5')
+  test_h5  = h5py.File(test_h5_path, 'w')
+  recursively_save_dict_contents_to_group(test_h5, '/', test_data)
+  test_h5.flush()
+  test_h5.close()
+  print('Did write {}'.format(test_h5_path))
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(
+      description='Create hdf5 files for cifar 10 pickle files.')
+  parser.add_argument('--data_pkl', nargs='?', type=str, required=True,
+      help='Directory containing cifar10 pkl files.')
+  parser.add_argument('--data_h5', type=str, nargs='?', required=True,
+      help='Directory where the h5 file should be saved.')
+
+  args = parser.parse_args()
+  pp = pprint.PrettyPrinter(indent=4)
+  pp.pprint(args)
+
+  main(args.data_pkl, args.data_h5)
+
---
+++ cifar_data_utils/noise_generator.py
@@ -0,0 +1,137 @@
+import numpy as np
+import argparse
+import csv
+import h5py
+import os
+import pprint
+import time
+import types
+
+from sklearn.metrics import confusion_matrix
+
+
+def recursively_save_dict_contents_to_group(h5file, path, dic):
+  """
+  Take an already open HDF5 file and insert the contents of a dictionary
+  at the current path location. Can call itself recursively to fill
+  out HDF5 files with the contents of a dictionary.
+  """
+  assert type(dic) is types.DictionaryType, "must provide a dictionary"
+  assert type(path) is types.StringType, "path must be a string"
+  assert type(h5file) is h5py._hl.files.File, "must be an open h5py file"
+  for key in dic:
+    assert type(key) is types.StringType, \
+    'dict keys must be strings to save to hdf5'
+    if type(dic[key]) in (np.int64, np.float64, types.StringType):
+      h5file[path + key] = dic[key]
+      assert h5file[path + key].value == dic[key], \
+        'The data representation in the HDF5 file does not match the ' \
+            'original dict.'
+    if type(dic[key]) is np.ndarray:
+      h5file[path + key] = dic[key]
+      assert np.array_equal(h5file[path + key].value, dic[key]), \
+          'The data representation in the HDF5 file does not match the ' \
+              'original dict.'
+    elif type(dic[key]) is types.DictionaryType:
+      recursively_save_dict_contents_to_group(
+          h5file, path + key + '/', dic[key])
+
+def create_noise_map(noise_csv, num_labels=10):
+  ''' Returns a dictionary with each key(label) mapped to a list of labels(keys)
+  with the probability that the source label (key) can be confused as the target
+  label(value).
+  '''
+  noise_map = {}
+  with open(noise_csv) as noise_csv_f:
+    noise_csv_reader = csv.DictReader(noise_csv_f)
+    for i in xrange(num_labels):
+      noise_map[i] = []
+
+    # Create the noise map
+    for row in noise_csv_reader:
+      if int(row['new_label']) >= 0:
+        old_label, new_label = int(row['org_label']), int(row['new_label'])
+        prob = float(row['prob'])
+        noise_map[old_label].append((new_label, prob))
+
+  return noise_map
+
+def get_random_noise_prob(noise_csv, num_labels=10):
+  noise = [0]*num_labels
+  with open(noise_csv) as noise_csv_f:
+    noise_csv_reader = csv.DictReader(noise_csv_f)
+    for row in noise_csv_reader:
+      if int(row['new_label']) < 0:
+        noise[int(row['org_label'])] = float(row['prob'])
+
+  return noise
+
+def get_confusion_matrix(y, noisy_y, num_labels=10):
+  ''' Get the confusion matrix for y and noisy_y labels.
+  '''
+  labels = [i for i in xrange(num_labels)]
+  conf = confusion_matrix(y, noisy_y, labels)
+  return conf
+
+def add_noise(y, noise_csv, num_labels=10):
+  noise_map = create_noise_map(noise_csv, num_labels=num_labels)
+  noisy_y = np.array(y, dtype=int)
+  # Add csv defined confusion noise
+  for i in xrange(y.shape[0]):
+    org, new_label = y[i][0], -1
+    p, total_p = np.random.rand(), 0
+    for noise_label, noise_prob in noise_map[org]:
+      total_p += p
+      if total_p < noise_prob:
+        new_label = noise_label
+        break
+    new_label = org if new_label == -1 else new_label    
+    noisy_y[i] = new_label
+
+  # Add random noise 
+  random_noise_prob = get_random_noise_prob(noise_csv, num_labels=num_labels)
+  for i in xrange(y.shape[0]):
+    # Only add random noise to clean samples i.e. for samples which we haven't
+    # already added noise to
+    if y[i, 0] == noisy_y[i, 0] and \
+        np.random.rand() < random_noise_prob[y[i, 0]]:
+      noisy_y[i, 0] = np.random.randint(num_labels)
+
+  assert(y.shape == noisy_y.shape)
+  return y, noisy_y
+
+def main(data_h5, new_data_h5, noise_csv):
+  data_h5f = h5py.File(data_h5, 'r')  
+  X = np.array(data_h5f['data'])
+  y = np.array(data_h5f['label'], dtype=int)
+  data_h5f.close()
+
+  org_y, noisy_y = add_noise(y, noise_csv, num_labels=10)
+  new_h5 = os.path.join(os.path.dirname(data_h5), new_data_h5)
+  new_h5_dict = {'data': X, 'label': org_y, 'noisy_label': noisy_y}
+  new_h5_f = h5py.File(new_h5, 'w')
+  recursively_save_dict_contents_to_group(new_h5_f, '/', new_h5_dict)
+  new_h5_f.flush()
+  new_h5_f.close()
+  conf = get_confusion_matrix(org_y, noisy_y, num_labels=10)
+  print("Confusion matrix for noisy labels:")
+  print(conf)
+  print("Did write file to {}".format(new_h5))
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(
+      description='Add noise to mnist labels.')
+  parser.add_argument('--data_h5', nargs='?', type=str, const=1,
+      required=True, default='', help='h5 file containing the original labels')
+  parser.add_argument('--new_data_h5', nargs='?', type=str, const=1,
+      required=True, default='', help='h5 file containing the new noisy labels')
+  parser.add_argument('--noise_csv', nargs='?', type=str, const=1,
+      required=True, default='', help='csv containing the noise parameters.')
+  args = parser.parse_args()
+  pp = pprint.PrettyPrinter(indent=4)
+  pp.pprint(args)
+  
+  np.random.seed(int(time.time()))
+
+  main(args.data_h5, args.new_data_h5, args.noise_csv)
+
